bc09f9fbea14cf4a9e1c2fb252d3aeed5afc9e12
shiva <shivaramnnp@gmail.com>
2026-05-10 10:48:26 +0530

    Fix Room migration crash by incrementing database version

--- app/src/main/java/com/shiva/quickMart/database/AppDatabase.kt
+++ app/src/main/java/com/shiva/quickMart/database/AppDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.shiva.quickMart.models.CartItem
 
-@Database(entities = [CartItem::class], version = 1, exportSchema = false)
+@Database(entities = [CartItem::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun cartDao(): CartDao
 
